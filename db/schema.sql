d5902611631e70baf0e515873a5fa1caa2e8eec5
theon87 <theonfrancis17@gmail.com>
2024-10-02 10:07:04 -0400

    adds empty query.sql file

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+\c movie_db;
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  movie_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  movie_id INTEGER NOT NULL,
+  review TEXT,
+  FOREIGN KEY (movie_id)
+  REFERENCES movies(id)
+  ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies (movie_name)
+VALUES ('The Dark Knight'),
+       ('The Matrix'),
+       ('Fight Club');
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, 'Heath Ledger had an amazing performance in his take of iconic character of The Joker.'),
+       (2, 'A mind bending concept which utilized new technologies and techniques in its time.'),
+       (3, 'We will not talk about this movie.');
